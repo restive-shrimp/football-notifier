88478ecd59c7641e7bc7922152a6300c55e36f1b
Restive_ <leszczynski.ps@gmail.com>
2021-09-12 23:30:06 +1000

    Update to mail to use flask-mail instead of Gmail API.

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 from apiclient.discovery import build
 import configparser
 import json
-
+from flask_mail import Mail, Message
 from bin import utils
 from bin.weather import Weather
 from bin.mailmanager import MailManager
 
 
 app = Flask(__name__)
-
 #Load config.ini file.
 config = configparser.ConfigParser()
 config.read('./config/config.ini')
@@ -17,7 +16,7 @@ config.read('./config/config.ini')
 
 def callFromCron(*args, **kwargs):
   """Function checking if the call was made by cron.
-  
+
   Returns:
     True if headers are from cron job.
   """
@@ -29,39 +28,59 @@ def callFromCron(*args, **kwargs):
 @app.route('/')
 def main():
   """Main page of the football notifier.
-  
+
   Returns:
     A redirection to a homepage of a Google Group.
   """
-  return redirect(config['REDIRECTS']['googleGroup'], code=302)
+  # Temporarily disabled
+  #return redirect(config['REDIRECTS']['googleGroup'], code=302)
+  return render_template("404.html")
 
-@app.errorhandler(404) 
-# inbuilt function which takes error as parameter 
-def not_found(e): 
+@app.errorhandler(404)
+# inbuilt function which takes error as parameter
+def not_found(e):
   """404 error handler.
 
   Returns:
     A 404 html template.
   """
-  return render_template("404.html") 
-    
+  return render_template("404.html")
+
 @app.route('/mail/<weekday>')
 def sendMail(weekday):
   """This is a page which cron job requests to send an email.
-  If 'today' is defined in config.ini, and call was made from cron, 
+  If 'today' is defined in config.ini, and call was made from cron,
   get the current forecast and send an email. Otherwise redirect.
 
-  Args: 
+  Args:
   weekday from url
-  
+
   Returns:
     A message content of the email or redirection to Google
   """
   if callFromCron():
-    m = MailManager(config, weekday)
+    #Config email
+    mail = initEmail()
+    m = MailManager(config, weekday, mail)
+    m.sendEmail()
     return m.createEmailMessageContent()
   else:
   	return redirect(url_for('main'))
+
+def initEmail():
+  """Initiating email configuration with data from config.ini.
+
+  Returns:
+    Configured flask_mail Mail object.
+  """
+  emailConfig = config['EMAIL']
+  app.config['MAIL_SERVER']= emailConfig['MAIL_SERVER']
+  app.config['MAIL_PORT'] = emailConfig['MAIL_PORT']
+  app.config['MAIL_USERNAME'] = emailConfig['MAIL_USERNAME']
+  app.config['MAIL_PASSWORD'] = emailConfig['MAIL_PASSWORD']
+  app.config['MAIL_USE_TLS'] = emailConfig.getboolean('MAIL_USE_TLS')
+  app.config['MAIL_USE_SSL'] = emailConfig.getboolean('MAIL_USE_SSL')
+  return Mail(app)
 
 
 if __name__ == '__main__':
